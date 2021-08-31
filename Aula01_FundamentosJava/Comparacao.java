fb5b1863342f442eb1acf1546c9555edcdb1eae1
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-08-31 18:58:03 -0300

    adicionando exercícios de fundamentos java

---
+++ Aula01_FundamentosJava/Comparacao.java
@@ -0,0 +1,13 @@
+/*
+*   ## 
+*   + Crie uma classe chamada Comparacao e made criar o método main.
+*   + No método main, implemente um programa que possui trÊs variaáveis locais: x, y e z.
+*     Em seguida imprima os números em ordem crescente.
+*   ##
+*/
+
+package Aula01_FundamentosJava;
+
+public class Comparacao {
+    
+}
---
+++ Aula01_FundamentosJava/Fatorial.java
@@ -0,0 +1,14 @@
+/*
+*   ## 
+*   + Crie uma classe chamada Fatorial e mande criar o método main.
+*   + No método main, implemente um programa que lê um número do teclado e em seguida imprime
+*     o seu fatorial.
+*   DICA: Procure implementar isso utilizando alguma estrutura de laço
+*   ##
+*/
+
+package Aula01_FundamentosJava;
+
+public class Fatorial {
+    
+}

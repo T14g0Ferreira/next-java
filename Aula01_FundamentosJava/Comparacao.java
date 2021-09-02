ee82aab485f6a7290eb18ee4aa6f8ca7696625d7
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-02 01:26:48 -0300

    números em ordem decrescente

--- Aula01_FundamentosJava/Comparacao.java
+++ Aula01_FundamentosJava/Comparacao.java
@@ -1,8 +1,8 @@
 /*
    ## 
    + Crie uma classe chamada Comparacao e mande criar o método main.
-   + No método main, implemente um programa que possui trÊs variaáveis locais: x, y e z.
-     Em seguida imprima os números em ordem crescente.
+   + No método main, implemente um programa que possui três variáveis locais: x, y e z.
+     Em seguida imprima os números em ordem decrescente.
    ##
 */
 
@@ -10,6 +10,27 @@ package Aula01_FundamentosJava;
 
 public class Comparacao {
   public static void main(String[] args) {
+      int x = 8;
+      int y = 3;
+      int z = 7;
+
+      if(x > y){
+        if(y > z){
+          System.out.println("Ordem decrescente: " + x + " - " + y + " - " + z);
+        } else if(x > z){
+          System.out.println("Ordem decrescente: " + x + " - " + z + " - " + y);
+        } else {
+          System.out.println("Ordem decrescente: " + z + " - " + x + " - " + y);
+        }
+      } else {
+          if(y > z){
+            System.out.println("Ordem decrescente: " + y + " - " + x + " - " + z);
+          } else if(x > z){
+            System.out.println("Ordem decrescente: " + y + " - " + z + " - " + x);
+          } else {
+            System.out.println("Ordem decrescente: " + z + " - " + y + " - " + x);
+          }
+      }
     
 
     }

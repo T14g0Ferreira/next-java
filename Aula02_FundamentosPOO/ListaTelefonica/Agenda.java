1cc4f334cb68b78e67e5e3cf7a78dd2952cdf1a8
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-16 23:13:00 -0300

    Cria representação de um cliente

--- Aula02_FundamentosPOO/ListaTelefonica/Agenda.java
+++ Aula02_FundamentosPOO/ListaTelefonica/Agenda.java
@@ -1,19 +1,42 @@
-package Aula02_FundamentosPOO.ListaTelefonica;
+package Aula02_FundamentosPOO.listaTelefonica;
 
 import java.util.ArrayList;
 
 public class Agenda {
-    private String emailDeSincronizaco;
-    private ArrayList <String> contatos;
+    private String emailDeSincronizacao;
+    private ArrayList<Contato> contatos;
 
-    public Agenda(String emailDeSincronizaco, ArrayList <String> contatos ){
-        this.emailDeSincronizaco = emailDeSincronizaco;
+    public Agenda(String emailDeSincronizacao, ArrayList<Contato> contatos) {
+        this.emailDeSincronizacao = emailDeSincronizacao;
         this.contatos = contatos;
     }
 
-    public String getEmailDeSincronizacao(){
-        return this.emailDeSincronizaco;
+    public Agenda(String emailDeSincronizacao) {
+        this.emailDeSincronizacao = emailDeSincronizacao;
     }
 
-    public 
+    public void addContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public void removeContato(Contato contato) {
+        for (int i = 0; i < this.contatos.size(); i++) {
+            Contato contatoAtual = this.contatos.get(i);
+            if(contatoAtual.getNome().equals(contato.getNome())) {
+                this.contatos.remove(i);
+            }
+        }
+    }
+
+    public String getEmailDeSincronizacao() {
+        return this.emailDeSincronizacao;
+    }
+
+    public ArrayList<Contato> getContatos() {
+        return this.contatos;
+    }
+
+    public int totalContatos() {
+        return this.contatos.size();
+    }
 }

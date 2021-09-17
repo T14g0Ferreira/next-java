4e37690de84949321733a6df46af50f665866de7
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-16 23:27:50 -0300

    Representação de um Caixa Eletrônico

--- Aula02_FundamentosPOO/ContaBancaria/App.java
+++ Aula02_FundamentosPOO/ContaBancaria/App.java
@@ -1,18 +1,26 @@
-package Aula02_FundamentosPOO.ContaBancaria;
+package Aula02_FundamentosPOO.contaBancaria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Conta contaDoLucas = new Conta("456456-7", "0001", 10000000.0);
-        Conta contaBBDoLucas = new Conta("456456-7", "0001", 100.0);
-
-        // Saque
-        System.out.println("Saldo antes do saque: " + contaDoLucas.getSaldo());
-        contaDoLucas.debitarSaldo(1500.0);
-        System.out.println("Saldo depois do saque: " + contaDoLucas.getSaldo());
-
-        System.out.println("------------------");
-
-        System.out.println("Saldo antes do saque: " + contaBBDoLucas.getSaldo());
-        contaBBDoLucas.debitarSaldo(1500.0);
-        System.out.println("Saldo depois do saque: " + contaBBDoLucas.getSaldo());
+         // System.out.println(Conta.rendimento);
+    
+         Conta contaNuDoLucasC1 = new Conta("456456-7", "0001");
+         Conta contaNuDoLucasC2 = new Conta("456456-7", "0001", 10000000.0);
+         Conta contaBBDoLucas = new Conta("456456-7", "0001", 100.0);
+ 
+         
+         System.out.println(contaNuDoLucasC1.getSaldo());
+         System.out.println(contaNuDoLucasC1.numero);
+ 
+         contaNuDoLucasC2.debitarSaldo(1000.0);
+         System.out.println(contaNuDoLucasC2.getSaldo());
+ 
+ 
+         // // Saque
+         System.out.println("------------------");
+ 
+         System.out.println("Saldo antes do saque: " + contaBBDoLucas.getSaldo());
+         contaBBDoLucas.debitarSaldo(1500.0);
+         System.out.println("Saldo depois do saque: " + contaBBDoLucas.getSaldo());
     }
-}
+}         

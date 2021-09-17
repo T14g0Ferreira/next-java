1828becb5321cb6005d6c58f38197ac899a85994
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-16 23:28:51 -0300

    Cria representação de uma conta bancária simples

--- Aula02_FundamentosPOO/ContaBancaria/Conta.java
+++ Aula02_FundamentosPOO/ContaBancaria/Conta.java
@@ -1,18 +1,36 @@
-package Aula02_FundamentosPOO.ContaBancaria;
+package Aula02_FundamentosPOO.contaBancaria;
 
+// modificador de acesso - class - NomeDaClasse
 public class Conta {
+
+    // public static final String RENDIMENTO = "100";
+
+    // Atributos (caracteristicas)
+    // modificador de acesso - tipo do atributo - nomeDoPadrao
+    // protected String numero;
     public String numero;
     public String agencia;
     private Double saldo;
 
-    public Conta(String numero, String agencia, Double saldo) {
+    // Construtores
+    // modificador de acesso - NomeDaClasse - parametros
+    public Conta(String numero, String agencia, Double saldo) { // Construtor 1
         this.numero = numero;
         this.agencia = agencia;
         this.saldo = saldo;
     }
 
+    // modificador de acesso - NomeDaClasse - parametros
+    public Conta(String numero, String agencia) { // Construtor 1
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = 0.0;
+    }
+
+    // Métodos (Ações)
+    // modificador de acesso - tipo de retorno - NomeDaClasse - parametros
     public void debitarSaldo(Double saque) {
-        if (saldo != null && saldo > 0 && saldo >= saque) {
+        if (podeSacar(saque)) {
             saldo -= saque;
         } else {
             System.out.println("Não há saldo suficiente!!!");
@@ -21,9 +39,14 @@ public class Conta {
 
     public void creditarSaldo(Double valor) {
         saldo += valor;
+        System.out.println(saldo);
     }
 
-    protected Double getSaldo(){
+    private boolean podeSacar(Double saque) {
+        return saldo != null && saldo > 0 && saldo >= saque;
+    }
+
+    public Double getSaldo() {
         return saldo;
     }
 }

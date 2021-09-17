4b13e6c96e73635f532852e6929821db4a89e8f8
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-16 23:07:45 -0300

    Indicação do horário real do sistema

---
+++ Aula01_FundamentosJava/exerciciosExtras/horaDoSistema.java
@@ -0,0 +1,11 @@
+package aula01_FundamentosJava.exerciciosExtras;
+
+import java.util.Date;
+
+public class horaDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é: ");
+        System.out.println(relogio.toString());
+    }
+}

c9d85626c58e8e91e825785b5a2f5dfd5714dc66
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-27 22:52:47 -0300

    Cria representação de uma porta de quarto

---
+++ Aula03_HerancaPolimorfismo/Casa/PortaQuarto.java
@@ -0,0 +1,14 @@
+package Aula03_HerancaPolimorfismo.Casa;
+
+public class PortaQuarto extends Porta{
+    
+    @Override
+    public void abrir(){
+
+    }
+
+    @Override
+    public void fechar(){
+        
+    }
+}

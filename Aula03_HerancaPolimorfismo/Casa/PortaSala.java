212a73ad5829721e7e6c9e16afe1b7ec7fa6b17d
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-27 22:53:01 -0300

    Cria representação de uma porta da sala

---
+++ Aula03_HerancaPolimorfismo/Casa/PortaSala.java
@@ -0,0 +1,14 @@
+package Aula03_HerancaPolimorfismo.Casa;
+
+public class PortaSala extends Porta{
+    
+    @Override
+    public void abrir(){
+
+    }
+
+    @Override
+    public void fechar(){
+        
+    }
+}

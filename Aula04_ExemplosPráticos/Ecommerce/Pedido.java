2c8721db501cfbd14c92fdda4a511f44a666ecbe
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:06:48 -0300

    representação do pedido feito pelo cliente

---
+++ Aula04_ExemplosPráticos/Ecommerce/Pedido.java
@@ -0,0 +1,12 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Pedido {
+    private String dataPedido;
+    private boolean status;
+    //private item itemPedido() = new itemPedido();
+    private double valorTotal;
+
+    public void statusPedido(){
+        
+    }
+}

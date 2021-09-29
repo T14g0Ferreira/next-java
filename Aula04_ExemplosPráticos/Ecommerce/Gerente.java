54df4e2df36e368ba3d5681f0903cc38beca1306
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:04:47 -0300

    Cria representação de um Gerente

---
+++ Aula04_ExemplosPráticos/Ecommerce/Gerente.java
@@ -0,0 +1,16 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Gerente {
+
+    public void listaFuncionario(){
+
+    }
+
+    public void listaCliente(){
+        
+    }
+    
+    public void criarFuncionario(){
+        
+    }
+}

214b77ee902553176d8754f2b9494ab4eee2a913
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:04:17 -0300

    Representação de um endereço de cliente

---
+++ Aula04_ExemplosPráticos/Ecommerce/Endereco.java
@@ -0,0 +1,13 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Endereco {
+    private String rua;
+    private int numero;
+    private String cep;
+    private String complemento;
+    private int telefone;
+    private int cidade;
+    private String bairro;
+
+
+}

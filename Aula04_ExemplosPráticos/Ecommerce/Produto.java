088baad8331e39bf5689d71caa9614b69fa6808e
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:05:23 -0300

    Cria representação de um produto

---
+++ Aula04_ExemplosPráticos/Ecommerce/Produto.java
@@ -0,0 +1,10 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Produto {
+    
+    private double peso;
+    private int quantidade;
+    private String nome;
+    private double valor;
+    private String descricao;
+}

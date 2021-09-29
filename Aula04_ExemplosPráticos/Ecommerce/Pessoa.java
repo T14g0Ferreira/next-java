c577e3309c1fd7c6e553da8d37b14658037e323a
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:05:06 -0300

    Cria representação de uma pessoa

---
+++ Aula04_ExemplosPráticos/Ecommerce/Pessoa.java
@@ -0,0 +1,13 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private int matricula;
+    private String nomeUsuario;
+    private String cargo;
+    private double salario;
+    private boolean status;
+    private String senha;
+
+}

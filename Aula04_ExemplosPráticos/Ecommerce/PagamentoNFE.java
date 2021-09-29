1763d6af2f6a385b98e1cdeb57974815dbb361f5
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:06:19 -0300

    Pagamentos de Nota Fiscal Eletrônica

---
+++ Aula04_ExemplosPráticos/Ecommerce/PagamentoNFE.java
@@ -0,0 +1,11 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class PagamentoNFE {
+    
+    private double valor;
+    private String dataPagamento;
+
+    public void efetuarPagamento(){
+        
+    }
+}

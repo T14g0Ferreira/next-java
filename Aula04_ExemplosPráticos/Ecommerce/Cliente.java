9721f16e8aa320920b8394c06deb399009367656
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-09-29 02:03:58 -0300

    Cria representação de um cliente

---
+++ Aula04_ExemplosPráticos/Ecommerce/Cliente.java
@@ -0,0 +1,12 @@
+package Aula04_ExemplosPráticos.Ecommerce;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String nomeUsuario;
+    private String cargo;
+    private double salario;
+    private boolean status;
+    private String senha;
+    
+}
